6a18b461ecf1572a6b63ecf2d818a48188eee06b
Dan Cunningham <dmcunningham@spscommerce.com>
2021-03-01 14:49:42 -0600

    update deploy

--- deployment/codepipeline/scripts/package.sh
+++ deployment/codepipeline/scripts/package.sh
@@ -22,7 +22,7 @@ echo "packaging lambda requirements"
 cd "$BUILDPATH/src"
 # python3.8 -m pip install --upgrade pip 
 pip install --use-feature=2020-resolver -r requirements.txt -t .
-zip -qr "package.zip" *
+zip -r "package.zip" * > /dev/null
 
 # upload package to S3
 echo "uploading lambda package to S3"
