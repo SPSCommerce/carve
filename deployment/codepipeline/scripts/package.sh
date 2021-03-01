48a0ade79eaf3eef23f7d454c8bf66d072e23c20
Dan Cunningham <dmcunningham@spscommerce.com>
2021-03-01 14:40:15 -0600

    update deploy

--- deployment/codepipeline/scripts/package.sh
+++ deployment/codepipeline/scripts/package.sh
@@ -1,11 +1,15 @@
+# update AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+./aws/install
 
-# GITHUBCURL="https://api.github.com/repos/anotherhobby/carve/commits"
-# GITTOKEN=$(aws secretsmanager get-secret-value --secret-id $GIT_TOKEN | jq -r '.SecretString' | jq -r '.token')
-# LATESTSHA=$(curl --location --request GET $GITHUBCURL --header "Authorization: Bearer ${GITTOKEN}" | jq -r '.[1].sha')
+GITHUBCURL="https://api.github.com/repos/anotherhobby/carve/commits"
+GITTOKEN=$(aws secretsmanager get-secret-value --secret-id $GIT_TOKEN | jq -r '.SecretString' | jq -r '.token')
+LATESTSHA=$(curl --location --request GET $GITHUBCURL --header "Authorization: Bearer ${GITTOKEN}" | jq -r '.[1].sha')
 
 # # clean up previous deployment files
-# echo "cleaning up prevous deployments"
-# aws s3 rm s3://$DEPLOYMENT_BUCKET/packages/ --recursive --exclude "${LATESTSHA}/*"
+echo "cleaning up prevous deployments"
+aws s3 rm s3://$DEPLOYMENT_BUCKET/packages/ --recursive --exclude "${LATESTSHA}/*"
 
 # upload deployment state machine definition
 echo "uploading state machine definition to S3"
@@ -16,9 +20,8 @@ aws s3 cp "$BUILDPATH/deployment/steps-carve-deployment.json" \
 # package lambda requirements
 echo "packaging lambda requirements"
 cd "$BUILDPATH/src"
-# python3.8 -m pip install --upgrade pip
-# --use-feature=2020-resolver
-pip install -r requirements.txt -t .
+# python3.8 -m pip install --upgrade pip 
+pip install --use-feature=2020-resolver -r requirements.txt -t .
 zip -qr "package.zip" *
 
 # upload package to S3
