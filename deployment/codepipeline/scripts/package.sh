4ee81e14be5560f62a5663fe95be1088c93673f9
Dan Cunningham <dmcunningham@spscommerce.com>
2021-03-01 14:24:21 -0600

    update buld

--- deployment/codepipeline/scripts/package.sh
+++ deployment/codepipeline/scripts/package.sh
@@ -1,24 +1,28 @@
 
-GITHUBCURL="https://api.github.com/repos/anotherhobby/carve/commits"
-GITTOKEN=$(aws secretsmanager get-secret-value --secret-id $GIT_TOKEN | jq -r '.SecretString' | jq -r '.token')
-LATESTSHA=$(curl --location --request GET $GITHUBCURL --header "Authorization: Bearer ${GITTOKEN}" | jq -r '.[1].sha')
+# GITHUBCURL="https://api.github.com/repos/anotherhobby/carve/commits"
+# GITTOKEN=$(aws secretsmanager get-secret-value --secret-id $GIT_TOKEN | jq -r '.SecretString' | jq -r '.token')
+# LATESTSHA=$(curl --location --request GET $GITHUBCURL --header "Authorization: Bearer ${GITTOKEN}" | jq -r '.[1].sha')
 
-# clean up previous deployment files
-echo "cleaning up prevous deployments"
-aws s3 rm s3://$DEPLOYMENT_BUCKET/packages/ --recursive --exclude "${LATESTSHA}/*"
+# # clean up previous deployment files
+# echo "cleaning up prevous deployments"
+# aws s3 rm s3://$DEPLOYMENT_BUCKET/packages/ --recursive --exclude "${LATESTSHA}/*"
 
 # upload deployment state machine definition
 echo "uploading state machine definition to S3"
-cd "$BUILDPATH/deployment/"
-aws s3 cp "steps-carve-deployment.json" s3://$DEPLOYMENT_BUCKET/carve/packages/$GITSHA/ --metadata GIT_SHA=$CODEBUILD_SOURCE_VERSION
+aws s3 cp "$BUILDPATH/deployment/steps-carve-deployment.json" \
+    s3://$DEPLOYMENT_BUCKET/carve/packages/$GITSHA/ \
+    --metadata GIT_SHA=$CODEBUILD_SOURCE_VERSION
 
 # package lambda requirements
 echo "packaging lambda requirements"
 cd "$BUILDPATH/src"
 # python3.8 -m pip install --upgrade pip
-pip install --use-feature=2020-resolver -r requirements.txt -t .
+# --use-feature=2020-resolver
+pip install -r requirements.txt -t .
 zip -qr "package.zip" *
 
 # upload package to S3
 echo "uploading lambda package to S3"
-aws s3 cp "package.zip" s3://$DEPLOYMENT_BUCKET/carve/packages/$GITSHA/ --metadata GIT_SHA=$CODEBUILD_SOURCE_VERSION
+aws s3 cp "package.zip" \
+    s3://$DEPLOYMENT_BUCKET/carve/packages/$GITSHA/ \
+    --metadata GIT_SHA=$CODEBUILD_SOURCE_VERSION
