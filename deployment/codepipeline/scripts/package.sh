f26a4c6a2d06a9160a32ded3d1c6d36f15d98693
Dan Cunningham <dmcunningham@spscommerce.com>
2021-03-01 21:23:38 -0600

    update build

--- deployment/codepipeline/scripts/package.sh
+++ deployment/codepipeline/scripts/package.sh
@@ -24,7 +24,7 @@ echo "packaging lambda requirements"
 cd "$BUILDPATH/src"
 # python3.8 -m pip install --upgrade pip 
 pip install -r requirements.txt -t .
-zip -qr "package.zip" * > /dev/null
+zip -r "package.zip" * > /dev/null
 
 # upload package to S3
 echo "uploading lambda package to S3"
@@ -38,7 +38,7 @@ mkdir ../layer
 cd ../layer
 # python3.8 -m pip install --upgrade pip 
 pip install -r ../src/graph_layer_requirements.txt -t .
-zip -qr "graph_layer_package.zip" * > /dev/null
+zip -r "graph_layer_package.zip" * > /dev/null
 # upload to S3
 echo "uploading lambda layer package to S3"
 aws s3 cp "graph_layer_package.zip" \
