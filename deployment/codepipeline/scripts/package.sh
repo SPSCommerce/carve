c5ae50e1e68d65fcc251f27ba96b9c65b968fee9
Dan Cunningham <danielmcunningham@gmail.com>
2021-03-13 17:26:29 -0600

    update deploy

--- deployment/codepipeline/scripts/package.sh
+++ deployment/codepipeline/scripts/package.sh
@@ -11,14 +11,8 @@ LATESTSHA=$(curl --location --request GET $GITHUBCURL --header "Authorization: B
 
 # # clean up previous deployment files
 echo "cleaning up prevous deployments"
-aws s3 rm s3://$DEPLOYMENT_BUCKET/carve/packages/ --recursive --exclude "${LATESTSHA}/*"
-aws s3 rm s3://$DEPLOYMENT_BUCKET/carve/layer_packages/ --recursive --exclude "${LATESTSHA}/*"
-aws s3 rm s3://$DEPLOYMENT_BUCKET/carve/graph_layer_packages/ --recursive --exclude "${LATESTSHA}/*"
-aws s3 rm s3://$DEPLOYMENT_BUCKET/carve/graph_layer_package/ --recursive --exclude "${LATESTSHA}/*"
-
-# future
 # aws s3 rm s3://$DEPLOYMENT_BUCKET/requirements_packages/ --recursive --exclude "${LATESTSHA}/*"
-# aws s3 rm s3://$DEPLOYMENT_BUCKET/lambda_packages/ --recursive --exclude "${LATESTSHA}/*"
+aws s3 rm s3://$DEPLOYMENT_BUCKET/lambda_packages/ --recursive --exclude "${LATESTSHA}/*"
 aws s3 rm s3://$DEPLOYMENT_BUCKET/step_functions/ --recursive --exclude "${LATESTSHA}/*"
 
 # upload deployment state machine definition
@@ -46,14 +40,14 @@ aws s3 cp "package.zip" \
     s3://$DEPLOYMENT_BUCKET/lambda_packages/$GITSHA/ \
     --metadata GIT_SHA=$CODEBUILD_SOURCE_VERSION
 
-# ######## only need to run when python requirments change/update
-echo "packaging lambda requirements layer"
-mkdir -p ../layer/python && cd ../layer
-pip install -r ../src/requirements.txt -t ./python
-zip -r "requirements_package.zip" * > /dev/null
-# upload to S3
-echo "uploading requirements lambda layer package to S3"
-aws s3 cp "requirements_package.zip" \
-    s3://$DEPLOYMENT_BUCKET/requirements_packages/$GITSHA/ \
-    --metadata GIT_SHA=$CODEBUILD_SOURCE_VERSION
+# # ######## only need to run when python requirments change/update
+# echo "packaging lambda requirements layer"
+# mkdir -p ../layer/python && cd ../layer
+# pip install -r ../src/requirements.txt -t ./python
+# zip -r "requirements_package.zip" * > /dev/null
+# # upload to S3
+# echo "uploading requirements lambda layer package to S3"
+# aws s3 cp "requirements_package.zip" \
+#     s3://$DEPLOYMENT_BUCKET/requirements_packages/$GITSHA/ \
+#     --metadata GIT_SHA=$CODEBUILD_SOURCE_VERSION
 
