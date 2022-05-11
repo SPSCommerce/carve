29755b9f831e3d6ea749c869f0a47515b910f3e1
Dan Cunningham <dmcunningham@spscommerce.com>
2022-05-11 13:52:54 -0500

    update lambda return to json

--- src/sf_scale_check_asg.py
+++ src/sf_scale_check_asg.py
@@ -18,7 +18,7 @@ def lambda_handler(event, context):
     # need to coerce data
     # data lives in "Payload" on first check, then when passed from the choice
     # state the data moves to the top of the json object
-    data = {'asg':{}}
+    data = {'asg': {}}
 
     if "Payload" in event:
         data['asg']['account'] = event['Payload']['asg']['account']
@@ -27,7 +27,7 @@ def lambda_handler(event, context):
         data['scale'] = event['Payload']['scale']
     else:
         data = event
-    
+ 
     credentials = aws_assume_role(carve_role_arn(data['asg']['account']), f"lookup-{data['asg']['name']}")
     response = aws_describe_asg(data['asg']['name'], data['asg']['region'], credentials)
 
@@ -48,8 +48,7 @@ def lambda_handler(event, context):
     # SHOULD ALSO CATCH FAILURE HERE TO RETURN: SCALE_FAILED
 
     data['ScaleStatus'] = scale_status
-
-    return response
+    return json.dumps(data, default=str)
 
 
 if __name__ == "__main__":
