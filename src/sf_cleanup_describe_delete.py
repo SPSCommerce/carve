a0fec5aeaaa7cb73b1e2eff849c4c41d23f51c5c
Dan Cunningham <dmcunningham@spscommerce.com>
2022-09-26 15:47:31 -0500

    update describe delete event handling

--- src/sf_cleanup_describe_delete.py
+++ src/sf_cleanup_describe_delete.py
@@ -1,4 +1,3 @@
-import json
 from utils import carve_role_arn
 from aws import aws_assume_role, aws_describe_stack
 
@@ -7,17 +6,17 @@ def lambda_handler(event, context):
     '''
     Describe a CFN stack in an account/region and return the status
     '''
-    # payload comes in differently from the step function if it's a choise state
+    # payload comes in differently from the step function if it's a choice state
     print(event)
     try:
-        payload = event['Payload']['Input']
+        payload = event['Input']['Payload']
     except:
         payload = event['Payload']
 
     account = payload['Account']
     credentials = aws_assume_role(carve_role_arn(account), f"carve-deploy-{payload['Region']}")
     response = aws_describe_stack(
-        stackname=payload['StackId'], # deleted stacks require the stack id to be described
+        stackname=payload['StackId'], # deleted stacks require the stack id in order to be described
         region=payload['Region'],
         credentials=credentials
         )
